3a46ad8fee663ba5c7c9e0f87c205442efacbaca
Rob McGrail <rob@aichi.(none)>
2012-05-06 19:56:39 +1200

    adding random string maker

--- modules/string.rb
+++ modules/string.rb
@@ -1,10 +1,14 @@
 # Helpers added to String
 
 class String
+  def self.random
+    (0...8).map{65.+(rand(25)).chr}.join
+  end
+
   def parameterize(delimeter = '-')
     self.gsub(/[^a-z0-9\-_!?]+/i, delimeter).downcase
   end
-  
+
   def encrypt
     BCrypt::Password.create(self)
   end
