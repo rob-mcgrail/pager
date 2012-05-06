27b2e58495b05170d28b36e0b8c7127a8d21e4ab
Rob McGrail <rob@aichi.(none)>
2012-05-06 19:56:59 +1200

    main method

--- app/main.rb
+++ app/main.rb
@@ -1,5 +1,41 @@
 get '/?' do
-  title 'home'
-  flash[:success] = '<strong>Everything</strong> is fine.'
-  erb "<h1>hello</h1>"
+  title 'welcome!'
+  @csfr_token = String.random
+  session[:token] = @csfr_token
+  erb :home
+end
+
+post '/new' do
+  unless session[:token] == params['token']
+    flash[:error] = "You're a dick, or possibly you need cookies enabled"
+    redirect '/'
+  end
+
+  email = params[:email]
+  password params[:password]
+
+  @user = User.first(:email => email)
+
+  unless @user
+    @user = User.new(
+      :email => email
+    )
+    unless @user.save
+      flash[:error] = "Something went really wrong"
+      redirect '/'
+    end
+  end
+
+  @page = @user.pages.new(
+    :password => password.encrypt,
+    :title => Page.default_title,
+    :slug => Page.generate_slug,
+    :body => Page.default_body,
+  )
+  if @page.save
+    redirect "/pages/edit/#{@page.slug}"
+  else
+    flash[:error] = "Something went really wrong"
+    redirect '/'
+  end
 end
