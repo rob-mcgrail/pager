8ddf3c4ef3c0f0be71158119c7575b66e175c746
Rob McGrail <rob@aichi.(none)>
2012-05-06 20:37:36 +1200

    dubious helpers

--- app/page.rb
+++ app/page.rb
@@ -11,4 +11,23 @@ class Page
   property :body,             Text,     :length => 0..100_000, :required => true
 
   belongs_to :user, :required => true
+
+  def self.default_title
+    'I need a title!'
+  end
+
+
+  def self.default_body
+    'A tutorial of how to use the site would go here'
+  end
+
+
+  def self.generate_slug
+    str = String.random
+    if self.first(:slug => str)
+      self.generate_slug
+    else
+      str
+    end
+  end
 end
