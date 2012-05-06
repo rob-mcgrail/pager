79b55c9ce3e2b56d7d0dc991ee4baef6e34d9545
Rob McGrail <rob@aichi.(none)>
2012-05-06 20:05:39 +1200

    bcrypt helpers seem broken in 1.9

--- modules/string.rb
+++ modules/string.rb
@@ -9,11 +9,7 @@ class String
     self.gsub(/[^a-z0-9\-_!?]+/i, delimeter).downcase
   end
 
-  def encrypt
-    BCrypt::Password.create(self)
-  end
-
-  def make_matchable
-    BCrypt::Password.new(self)
-  end
+#  def make_matchable
+#    BCrypt::Password.new(self)
+#  end
 end
